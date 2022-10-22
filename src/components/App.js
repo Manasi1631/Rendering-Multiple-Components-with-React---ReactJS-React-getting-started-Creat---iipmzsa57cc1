5be0c1bc3d5425333091f10898a4f0775b0e6371
Manasi1631 <110302624+Manasi1631@users.noreply.github.com>
2022-10-22 15:07:44 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,15 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main"><h1 data-ns-test="project-name">E-Commerce Website</h1>
+        <p data-ns-test="project-description">
+        React is a great JavaScript framework to build an e-commerce web app. It
+        has a big & active community, is component-based, supports server-side
+        rendering, uses virtual DOM, just to mention a few. These features make
+        React. js the perfect choice for a fast, scalable, and user-friendly
+        e-commerce website.
+      </p>
+    </div>
   )
 }
 
